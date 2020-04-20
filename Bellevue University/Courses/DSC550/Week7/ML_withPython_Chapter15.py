081672dff632d923f6c0d75e56b837c65c28d209
safarie1103 <54446804+safarie1103@users.noreply.github.com>
2020-04-20 14:41:08 -0500

    04202020

---
+++ Bellevue University/Courses/DSC550/Week7/ML_withPython_Chapter15.py
@@ -0,0 +1,12 @@
+#%%
+
+# Load libraries
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+
+
